411834803202aa9204fbd3a5de67bab69a7c6fd5
Raí Joia <megac154@gmail.com>
2024-01-12 12:38:13 -0300

    rxjs(interface): interfaces ok

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { LivrosResultado } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root',
